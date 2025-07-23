e577d872b0a44bd944a180cb99f5cdfe9a3f8670
gray-sin <gray.sin@kakaocorp.com>
2025-07-23 14:53:49 +0900

    day3: example 코드 완성(tavliy 검색 도구 붙이기)

--- day3/example.py
+++ day3/example.py
@@ -1,11 +1,27 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv() # .env 파일 로드
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature = 0)
 
 print(llm.invoke("Hello, how are you?"))
+
+from tavily import TavilyClient
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+response = tavily.search(query="Where does Messi play right now?", max_results=3)
+context = [{"url": obj["url"], "content": obj["content"]} for obj in response['results']]
+
+# You can easily get search result context based on any max tokens straight into your RAG.
+# The response is a string of the context within the max_token limit.
+
+response_context = tavily.get_search_context(query="Where does Messi play right now?", search_depth="advanced", max_tokens=500)
+
+# You can also get a simple answer to a question including relevant sources all with a simple function call:
+# You can use it for baseline
+response_qna = tavily.qna_search(query="Where does Messi play right now?")
 
 ### Index
 
