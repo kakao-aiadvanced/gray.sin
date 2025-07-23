09c787844a869e6e469fceade76c9b61826f76ee
gray-sin <gray.sin@kakaocorp.com>
2025-07-23 15:51:25 +0900

    1단계: workflow 수정

--- day3/workflow.py
+++ day3/workflow.py
@@ -27,39 +27,36 @@ def create_workflow():
     workflow = StateGraph(GraphState)
 
     # Define the nodes
-    workflow.add_node("websearch", web_search)  # web search
     workflow.add_node("retrieve", retrieve)  # retrieve
+    workflow.add_node("websearch", web_search)  # web search
     workflow.add_node("grade_documents", grade_documents)  # grade documents
     workflow.add_node("generate", generate)  # generate
+    workflow.add_node("grade_generation", grade_generation)  # grade generation v documents and question
 
     # Build graph
-    workflow.set_conditional_entry_point(
-        route_question,
-        {
-            "websearch": "websearch",
-            "vectorstore": "retrieve",
-        },
-    )
-
+    workflow.set_entry_point("retrieve")
     workflow.add_edge("retrieve", "grade_documents")
     workflow.add_conditional_edges(
         "grade_documents",
         decide_to_generate,
         {
-            "websearch": "websearch",
-            "generate": "generate",
+            "yes": "generate",
+            "no": "websearch",
         },
     )
-    workflow.add_edge("websearch", "generate")
+    workflow.add_edge("websearch", "grade_documents")
+    # TODO 최대 1회 로직 추가
+
+    workflow.add_edge("generate", "grade_generation")
     workflow.add_conditional_edges(
-        "generate",
+        "grade_generation",
         grade_generation_v_documents_and_question,
         {
-            "not supported": "generate",
-            "useful": END,
-            "not useful": "websearch",
+            "yes": END,
+            "no": "generate",
         },
     )
+
 
     # Compile
     return workflow.compile() 
